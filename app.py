5dbc1c30bc1ca273b4bd5fc64d5f8d7f502f6913
Nhan2hcd <139828451+Nhan2hcd@users.noreply.github.com>
2023-07-20 02:46:32 +0000

    20/07/2023

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app= Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Hello, Nhan'
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
